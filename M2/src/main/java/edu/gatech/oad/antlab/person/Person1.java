66a4a9fb35b951c299ca813075260bba4ab1a56d
anish <anishvisaria98@gmail.com>
2017-09-02 14:44:23 -0400

    implemented Person1.java methods

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Anish Visaria
  *  @version 1.1
  */
 public class Person1 {
@@ -30,8 +30,13 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		if (input == null) {
+			throw new IllegalArgumentException("input string cannot be null");
+		}
+		if (input.length() < 3) {
+			return input;
+		}
+	  return input.substring(2) + input.substring(0, 2);
 	}
 	
 	/**
